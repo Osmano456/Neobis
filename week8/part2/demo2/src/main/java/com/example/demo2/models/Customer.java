fd37b09133f18d1fe4ff7c900ca5324ec1cc634c
Weegang <osmanozyilmaz456@gmail.com>
2022-03-10 09:21:40 +0600

    added vulnerabilities.txt and authentication&authorization to app

---
+++ week8/part2/demo2/src/main/java/com/example/demo2/models/Customer.java
@@ -0,0 +1,69 @@
+package com.example.demo2.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String firstname;
+    private String lastname;
+    private int phonenum;
+
+    public Customer() {
+    }
+
+
+    public Customer(int id, String firstname, String lastname, int phonenum) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phonenum = phonenum;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(int phonenum) {
+        this.phonenum = phonenum;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", phonenum=" + phonenum +
+                '}';
+    }
+}
